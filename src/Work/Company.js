f9ff2f397650ef752cbb45d0ca62b99083077f25
smshen <thanatos1710@gmail.com>
2017-08-30 14:48:00 +0800

    change font-size of sidebar

--- src/Work/Company.js
+++ src/Work/Company.js
@@ -19,13 +19,15 @@ class Company extends Component {
       <div className='company container'>
         <div className='row'>
           <div className='col-6'>
-            <h2>{name}</h2>
-            <h4>{job}</h4>
-            <h6>{`${startDate} ~ ${endDate}`}</h6>
+            <h1>{name}</h1>
+            <h3>{job}</h3>
+            <h5>{`${startDate} ~ ${endDate}`}</h5>
             <a target='_blank' href={link}>Visit website</a>
           </div>
           <div className='col-6'>
-            <img src={imgSrc} height='200' width='200' alt='company'/>
+            <a target='_blank' href={link}>
+              <img src={imgSrc} height='200' width='200' alt='company'/>
+            </a>
           </div>
         </div>
       </div>
