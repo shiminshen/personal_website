b496b5230f616cce221d65718b1b777ae5a25395
smshen <thanatos1710@gmail.com>
2017-09-11 21:18:29 +0800

    add react-transition-group

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import {
   HashRouter as Router,
   Route,
+  Switch,
   Link
 } from 'react-router-dom'
+
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
 
@@ -19,22 +22,33 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className='App container-fluid'>
-          <div className="wrapper row align-items-center">
-            <ul className='sidebar col-4'>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/work'>Work</Link></li>
-              <li><Link to='/portfolio'>Portfolio</Link></li>
-              <li><Link to='/contact'>Contact</Link></li>
-            </ul>
-            <div className='main col-8'>
-              <Route exact path='/' component={Home}/>
-              <Route exact path='/work' component={Work}/>
-              <Route exact path='/portfolio' component={Portfolio}/>
-              <Route exact path='/contact' component={Contact}/>
+        <Route render={({ location }) => (
+          <div className='App container-fluid'>
+            <div className="wrapper row align-items-center">
+              <ul className='sidebar col-4'>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/work'>Work</Link></li>
+                <li><Link to='/portfolio'>Portfolio</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
+              </ul>
+              <TransitionGroup className='main col-8'>
+                <CSSTransition
+                  key={location.pathname}
+                  classNames='change-route'
+                  timeout={500}
+                >
+                  <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route exact path='/work' component={Work}/>
+                    <Route exact path='/portfolio' component={Portfolio}/>
+                    <Route exact path='/contact' component={Contact}/>
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
             </div>
           </div>
-        </div>
+          )}>
+        </Route>
       </Router>
       );
   }
